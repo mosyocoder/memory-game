2db6316e9a6924ef5e0fde959cc5a20e1e878e7e
mosyocoder <mosyocoder1@gmail.com>
2024-04-16 00:36:04 +0300

    tailwind init, image array mix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import "./App.css";
+import { images } from "./images";
+
+const remixArr = (arr) => {
+	const duplicatedArr = arr.flatMap((item) => [item, item]);
+
+	const shuffledArr = duplicatedArr.sort(() => Math.random() - 0.5);
+
+	return shuffledArr;
+};
 
 function App() {
-	return <div className="App">memory app</div>;
+	const arr = remixArr(images);
+
+	return (
+		<div className="bg-bg h-screen grid grid-cols-4">
+			{arr.map((item) => (
+				<div className="w-44 h-44 rounded-xl bg-white flex items-center justify-center">
+					<img className="w-40 h-40 rounded-md" src={item.image} alt="" />
+				</div>
+			))}
+		</div>
+	);
 }
 
 export default App;
---
+++ src/images.js
@@ -0,0 +1,63 @@
+import imgone from "./images/1.jpeg";
+import imgtwo from "./images/2.jpeg";
+import imgthree from "./images/3.jpeg";
+import imgfour from "./images/4.jpeg";
+import imgfive from "./images/5.jpeg";
+import imgsix from "./images/6.jpeg";
+import imgseven from "./images/7.jpeg";
+import imgeight from "./images/8.jpeg";
+import imgnine from "./images/9.jpeg";
+import imgten from "./images/10.jpeg";
+
+export const images = [
+	{
+		image: imgone,
+		matched: false,
+		opened: false,
+	},
+	{
+		image: imgtwo,
+		matched: false,
+		opened: false,
+	},
+	{
+		image: imgthree,
+		matched: false,
+		opened: false,
+	},
+	{
+		image: imgfour,
+		matched: false,
+		opened: false,
+	},
+	{
+		image: imgfive,
+		matched: false,
+		opened: false,
+	},
+	{
+		image: imgsix,
+		matched: false,
+		opened: false,
+	},
+	{
+		image: imgseven,
+		matched: false,
+		opened: false,
+	},
+	{
+		image: imgeight,
+		matched: false,
+		opened: false,
+	},
+	{
+		image: imgnine,
+		matched: false,
+		opened: false,
+	},
+	{
+		image: imgten,
+		matched: false,
+		opened: false,
+	},
+];
