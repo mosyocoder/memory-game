9ee40f0934faf4ea655d015aec392502b5b16f4f
mosyocoder <mosyocoder1@gmail.com>
2024-04-25 18:37:54 +0300

    turn card actions

--- src/App.js
+++ src/App.js
@@ -8,18 +8,11 @@ import "./App.css";
 function App() {
 	const arr = remixArr(images);
 
-	console.log(arr);
-
 	return (
 		<div className="relative bg-bg w-full h-screen flex sm:justify-center">
 			<div className="grid grid-cols-4-auto md:grid-cols-5-auto gap-3 lg:aspect-6/5 lg:mt-3 h-90-vh content-center justify-center m-2">
 				{arr.map((item) => (
-					<div className="">
-						<div className="f"></div>
-						<div className="b">
-							<img className="w-full" src={item.image} alt="" />
-						</div>
-					</div>
+					<Card key={item.id} item={item} />
 				))}
 			</div>
 		</div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Card({ item }) {
-	return <img className="aspect-square" src={item.image} alt="" />;
+	const [cardOne, setCardOne] = useState();
+	const [cardTwo, setCardTwo] = useState();
+
+	const turnCard = (id) => {
+		console.log("cardone = ", cardOne);
+		console.log("cardtwo = ", cardTwo);
+		if (!cardOne) {
+			setCardOne(id);
+		}
+		if (cardOne && !cardTwo) {
+			setCardTwo(id);
+		}
+	};
+
+	return (
+		<div onClick={() => turnCard(item.id)} id={item.id} className={`aspect-square relative cursor-pointer c ${cardOne === item.id || cardTwo === item.id ? "active" : ""}`}>
+			<div className={`f bg-white font-bold text-8xl transition-all ease-in-out backface-hidden w-full h-full absolute flex items-center justify-center `}>?</div>
+			<div className={`b bg-white transition-all ease-in-out backface-hidden rotate-y-180`}>
+				<img className="w-full" src={item.image} alt="" />
+			</div>
+		</div>
+	);
 }
 
 export default Card;
